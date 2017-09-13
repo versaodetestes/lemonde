ecda68b8eb1e0414f4120c1be017c4f8f6788ce4
Igor Prado <contato@igorprado.com.br>
2017-09-13 17:56:22 -0300

    home in progress

---
+++ assets/js/main-script.js
@@ -0,0 +1,6 @@
+// youtube cover player
+$('#youtubecover').click(function() {
+$('#ytplayer').show();
+$('#ytplayer').append('<iframe width="911" height="627" src="http://www.youtube.com/embed/9ZKEJ6OFCV4?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+$('#youtubecover').hide();
+});
--- index.php
+++ index.php
@@ -4,120 +4,148 @@
 	<meta charset="UTF-8">
 	<title>Le Monde Campolim</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/style-general.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 </head>
 <body>
 	<!-- HEADER -->
 	<div class="header">
 		<div class="sub-header">
-			
+			<div class="container">
+				<img src="assets/img/logo-crb.svg" class="logo-crb-header float-right">
+			</div>
 		</div>
 		<div class="main-header">
-			<div class="brand">
-				
+		<div class="container">
+			<div class="navbar">
+				<div class="brand">
+					<img src="assets/img/logo-lemonde.svg">
+				</div>
+				<div class="nav-header">
+					<ul>
+						<li><a href="">Empreendimento</a></li>
+						<li><a href="">Apartamentos</a></li>
+						<li><a href="">Lazer</a></li>
+						<li><a href="">Localização</a></li>
+						<li><a href="">Contato</a></li>
+					</ul>
+				</div>
 			</div>
-			<div class="nav nav-header">
-				<ul>
-					<li><a href="">Empreendimento</a></li>
-					<li><a href="">Apartamentos</a></li>
-					<li><a href="">Lazer</a></li>
-					<li><a href="">Localização</a></li>
-					<li><a href="">Contato</a></li>
-				</ul>
-			</div>
+
+		</div>
+
 		</div>
 	</div>
 	<!-- FIM HEADER -->
 	<!-- HERO -->
-	<div class="hero">
+	<div class="hero hero-home">
 		
 	</div>
 	<!-- FIM HEADER -->
 	<!-- EMPREENDIMENTO -->
-	<div class="container">
-		<div class="row">
-
-			<div class="about-building">
-
+	<div class="container about-building">
+		<div class="row justify-content-center">
 				<div class="title-group">
 					<h1>O empreendimento</h1>
-					<p>Tendência internacional de moradia</p>
+					<p class="lead">Tendência internacional de moradia</p>
 				</div>
-
-				<div class="feat-group">
-					<div class="feature">
-						Campolim, Sorocaba/SP
-					</div>
-					<div class="feature">
-						4 opções de planta
-					</div>
-					<div class="feature">
-						Quarta torre lançada
-					</div>
-					<div class="feature">
-						5 torres 
-					</div>					
+		</div>
+		<div class="row justify-content-center">
+			<div class="col-md-3">
+				<div class="feature align-middle">
+					Campolim, Sorocaba/SP
 				</div>
-
-				<div class="midia">
-					<div class="midia-text">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam repellendus assumenda ex deleniti voluptatibus.
-					</div>
-					<div class="midia-cta">
-						<a class="btn btn-cta">Saiba mais do empreendimento</a>
-					</div>
+			</div>
+			<div class="col-md-3">
+				<div class="feature">
+					4 opções de planta
 				</div>
-
+			</div>
+			<div class="col-md-3">
+				<div class="feature">
+					Quarta torre lançada
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="feature">
+					5 torres 
+				</div>					
 			</div>
 		</div>
 
+		<div class="row justify-content-center">
+			<div class="midia">
+				<div class="midia-player">
+					<img src="assets/img/youtube-cover.jpg" class="img-fluid" id="youtubecover">
+					<div id="ytplayer" style="display:none;">
+					</div>
+				</div>
+				<div class="midia-text">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam repellendus assumenda ex deleniti voluptatibus.
+				</div>
+				<div class="midia-cta">
+					<a class="btn btn-cta" href="./empreendimento">Veja mais informações</a>
+				</div>
+			</div>
+		</div>
+
+
+
 		<div class="row">
 
 			<div class="cards col-md-4">
 				<div class="card-block card-apto">
-					<span class="card-title">Apartamentos</span>
+				  <div class="card-info">
+					<h3 class="card-title">Apartamentos</h3>
 					<a class="btn btn-cta btn-inverse" href="./apartamentos">Escolha sua planta ideal</a>
+				  </div>
 				</div>
+ 				<span class="card-color"></span>
 			</div>
 
 			<div class="cards col-md-4">
-				<div class="card-block card-apto">
-					<span class="card-title">Lazer completo</span>
-					<a class="btn btn-cta btn-inverse" href="./lazer">Um mundo de possibilidades</a>
+				<div class="card-block card-lazer">
+					<div class="card-info">
+						<h3 class="card-title">Lazer completo</h3>
+						<a class="btn btn-cta btn-inverse" href="./lazer">Um mundo de possibilidades</a>
+					</div>
 				</div>
+ 				<span class="card-color"></span>
 			</div>
 
 			<div class="cards col-md-4">
-				<div class="card-block card-apto">
-					<span class="card-title">Localização</span>
-					<a class="btn btn-cta btn-inverse" href="./lazer">Explore seu novo endereço</a>
+				<div class="card-block card-localizacao">
+				  <div class="card-info">
+					<h3 class="card-title">Localização</h3>
+					<a class="btn btn-cta btn-inverse" href="./localizacao">Explore seu novo endereço</a>
+				  </div>
+				 
 				</div>
+ 				<span class="card-color"></span>
 			</div>
 
 		</div>
 
 		</div>
 		<div class="container-fluid">
-			<div class="testimonial">
-				<div class="testimonial-image">
+			<div class="row no-gutter justify-content-center testimonial">
+				<div class="col-md-2 testimonial-image">
 					
 				</div>
-				<div class="testimonial-text">
+				<div class="col-md-4 testimonial-text">
 					"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et aliquid fuga voluptatibus fugiat dignissimos facere autem nihil commodi amet ab sed, sequi minima incidunt, doloremque id earum laboriosam delectus ullam!"
 				</div>
 			</div>
 		</div>
 		<div class="container">
-			<div class="row">
-				<div class="stand">
-					<div class="stand-img">
-						<img src="">
-					</div>
-					<div class="stand-text">
-						<h3>Stand de vendas</h3>
+			<div class="row justify-content-center">
+				<div class="cta-stand media">
+						<img src="assets/img/cta-maquete.jpg" class="img-fluid d-flex align-self-start mr-3">
+					<div class="media-body stand-text">
+						<h3 class="cta-stand-title">Stand de vendas</h3>
 						<p>
 							Visite nosso Stand de vendas e aproveite para conhecer o Apartamento Decorado
 						</p>
-						<a href="./contato">Entre em contato</a>
+						<a href="./contato" class="btn btn-cta">Entre em contato</a>
 					</div>
 					
 				</div>
@@ -142,7 +170,7 @@
 							<p>
 								A CRB Construtora é uma empresa de Sorocaba, interior de São Paulo, que permanece há mais de 10 anos com as atividades focadas na cidade. Realiza empreendimentos de padrão elevado, inovadores e com foco na qualidade e entrega com diferenciais.
 							</p>
-							<a href="#">Saiba mais sobre nós</a>
+							<a href="#" class="btn">Saiba mais sobre nós</a>
 						</div>
 					</div>
 				</footer>
@@ -158,6 +186,10 @@ Política de Privacidade
 				</small>
 			</div>
 		</div>
+
+<script type="text/javascript" src="assets/js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="assets/js/main-script.js"></script>
+
 
 </body>
 </html>
