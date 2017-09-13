f00d0311210106b9210d076ce26f7786b863fdf8
Igor Prado <contato@igorprado.com.br>
2017-09-13 08:11:55 -0300

    Estrututra de index
    Estrutura da index está semi-completa

---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Le Monde Campolim</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style-general.css">
+</head>
+<body>
+	<!-- HEADER -->
+	<div class="header">
+		<div class="sub-header">
+			
+		</div>
+		<div class="main-header">
+			<div class="brand">
+				
+			</div>
+			<div class="nav nav-header">
+				<ul>
+					<li><a href="">Empreendimento</a></li>
+					<li><a href="">Apartamentos</a></li>
+					<li><a href="">Lazer</a></li>
+					<li><a href="">Localização</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<!-- FIM HEADER -->
+	<!-- HERO -->
+	<div class="hero">
+		
+	</div>
+	<!-- FIM HEADER -->
+	<!-- EMPREENDIMENTO -->
+	<div class="container">
+		<div class="row">
+
+			<div class="about-building">
+
+				<div class="title-group">
+					<h1>O empreendimento</h1>
+					<p>Tendência internacional de moradia</p>
+				</div>
+
+				<div class="feat-group">
+					<div class="feature">
+						Campolim, Sorocaba/SP
+					</div>
+					<div class="feature">
+						4 opções de planta
+					</div>
+					<div class="feature">
+						Quarta torre lançada
+					</div>
+					<div class="feature">
+						5 torres 
+					</div>					
+				</div>
+
+				<div class="midia">
+					<div class="midia-text">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam repellendus assumenda ex deleniti voluptatibus.
+					</div>
+					<div class="midia-cta">
+						<a class="btn btn-cta">Saiba mais do empreendimento</a>
+					</div>
+				</div>
+
+			</div>
+		</div>
+
+		<div class="row">
+
+			<div class="cards col-md-4">
+				<div class="card-block card-apto">
+					<span class="card-title">Apartamentos</span>
+					<a class="btn btn-cta btn-inverse" href="./apartamentos">Escolha sua planta ideal</a>
+				</div>
+			</div>
+
+			<div class="cards col-md-4">
+				<div class="card-block card-apto">
+					<span class="card-title">Lazer completo</span>
+					<a class="btn btn-cta btn-inverse" href="./lazer">Um mundo de possibilidades</a>
+				</div>
+			</div>
+
+			<div class="cards col-md-4">
+				<div class="card-block card-apto">
+					<span class="card-title">Localização</span>
+					<a class="btn btn-cta btn-inverse" href="./lazer">Explore seu novo endereço</a>
+				</div>
+			</div>
+
+		</div>
+
+		</div>
+		<div class="container-fluid">
+			<div class="testimonial">
+				<div class="testimonial-image">
+					
+				</div>
+				<div class="testimonial-text">
+					"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et aliquid fuga voluptatibus fugiat dignissimos facere autem nihil commodi amet ab sed, sequi minima incidunt, doloremque id earum laboriosam delectus ullam!"
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="stand">
+					<div class="stand-img">
+						<img src="">
+					</div>
+					<div class="stand-text">
+						<h3>Stand de vendas</h3>
+						<p>
+							Visite nosso Stand de vendas e aproveite para conhecer o Apartamento Decorado
+						</p>
+						<a href="./contato">Entre em contato</a>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<footer>
+					<div class="col-md-6">
+						<ul class="nav-footer">
+							<li><a href="./">Home</a></li>
+							<li><a href="./empreendimentos">Empreendimento</a></li>
+							<li><a href="./apartamentos">Apartamentos</a></li>
+							<li><a href="./lazer">Lazer</a></li>
+							<li><a href="./localização">Localiazação</a></li>
+							<li><a href="./contato">Contato</a></li>
+						</ul>
+					</div>
+					<div class="col-md-6">
+						<div class="about-company">
+							<img src="">
+							<p>
+								A CRB Construtora é uma empresa de Sorocaba, interior de São Paulo, que permanece há mais de 10 anos com as atividades focadas na cidade. Realiza empreendimentos de padrão elevado, inovadores e com foco na qualidade e entrega com diferenciais.
+							</p>
+							<a href="#">Saiba mais sobre nós</a>
+						</div>
+					</div>
+				</footer>
+			</div>
+		</div>
+		<div class="container-fluid">
+			<div class="memorial">
+				<small>
+					*Consulte memorial descritivo no stand de vendas.
+Perspectivas meramente ilustrativas. Consulte memorial descritivo nos stand de vendas. Incorporação conforme matrícula
+nº 25.522 do 2º Cartório de Registro de Imóveis de Sorocaba/SP.  Alvará de Licença nº 533/16SEMOB/DLCON/SEP 
+Política de Privacidade
+				</small>
+			</div>
+		</div>
+
+</body>
+</html>
