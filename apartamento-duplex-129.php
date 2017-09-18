b235bfd20c19a22047295718c4da968289796858
Igor Prado <contato@igorprado.com.br>
2017-09-18 18:02:52 -0300

    Duplex 129 - Start

---
+++ apartamento-duplex-129.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+	<meta charset="UTF-8">
+	<title>Duplex 129m² - Le Monde Campolim</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style-apartamentos.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+</head>
+<body>
+	<!-- HEADER -->
+	<?php include ("header.php"); ?>
+	<!-- FIM HEADER -->
+	<div class="container">
+
+	</div>
+
+		<!-- CTA -->
+		<?php include ("cta-stand.php"); ?>
+		<!-- FIM CTA -->
+		<!-- FOOTER -->
+		<?php include ("footer.php"); ?>
+		<!-- FIM FOOTER -->
+
+<script type="text/javascript" src="assets/js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="assets/js/main-script.js"></script>
+
+</body>
+</html>
