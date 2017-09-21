ff690b1cd493e5bf073c9705bbb67e68f7068459
Igor Prado <contato@igorprado.com.br>
2017-09-21 11:15:18 -0300

    Youtube HTTPS

--- assets/js/main-script.js
+++ assets/js/main-script.js
@@ -9,7 +9,7 @@
 // youtube cover player
 $('#youtubecover').click(function() {
 $('#ytplayer').show();
-$('#ytplayer').append('<iframe width="911" height="627" src="http://www.youtube.com/embed/9ZKEJ6OFCV4?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+$('#ytplayer').append('<iframe width="911" height="627" src="https://www.youtube.com/embed/9ZKEJ6OFCV4?autoplay=1" frameborder="0" allowfullscreen></iframe>');
 $('#youtubecover').hide();
 });
 
