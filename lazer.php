95f5e908500d19ea3069e43876f0e57a78f4a964
Igor Prado <contato@igorprado.com.br>
2017-09-28 17:11:31 -0300

    Grafia Lazer Completíssimo

--- lazer.php
+++ lazer.php
@@ -35,7 +35,7 @@
 		<div class="row justify-content-center">
 			<div class="col-md-8 text">
 				<div class="text-center">
-								<h2>Lazer completissímo</h2>
+								<h2>Lazer completíssimo</h2>
 				<span class="line line-blue"></span>	
 				</div>
 
