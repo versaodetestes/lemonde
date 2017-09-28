ddb8fa7411748c179fd36bc9eeb60d0f0cbebc24
Igor Prado <contato@igorprado.com.br>
2017-09-28 09:54:03 -0300

    Pagina Obrigado

---
+++ obrigado.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+	<meta charset="UTF-8">
+	<title>Contato - Le Monde Campolim</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style-contato.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+</head>
+<body>
+	<!-- HEADER -->
+	<?php include ("header.php"); ?>
+	<!-- FIM HEADER -->
+	<div class="container">
+		<div class="row justify-content-center">
+				<div class="title-group text-center">
+					<h1>Obrigado</h1>
+					<p >Recebemos sua mensagem e em breve um de nossos corretores entrará em contato.</p>
+					<span class="line line-blue"></span><br>
+				</div>
+		</div>
+		<div class="row justify-content-center text-center">
+			<div class="col-md-7">
+				<a href="https://crbconstrutora.com.br/apartamentos-na-planta-em-sorocaba#conteudo"><img src="assets/img/apartamentos-na-planta.jpg" class="img-fluid"></a>
+
+			</div>
+
+		</div>
+
+	</div>
+
+
+		<!-- FOOTER -->
+		<?php include ("footer.php"); ?>
+		<!-- FIM FOOTER -->
+
+<script type="text/javascript" src="assets/js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="assets/js/main-script.js"></script>
+</body>
+</html>	
