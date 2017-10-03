f77d6ab8cec9676275f935e46d1eaf4da37f9f3e
Igor Prado <contato@igorprado.com.br>
2017-10-03 17:57:01 -0300

    Remove Hotjar

--- footer.php
+++ footer.php
@@ -98,15 +98,3 @@ var google_remarketing_only = true;
 <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/882192095/?value=1.00&amp;currency_code=BRL&amp;label=t6y9COXCp3AQ393UpAM&amp;guid=ON&amp;script=0"/>
 </div>
 </noscript>
-
-<!-- Hotjar Tracking Code for crbconstrutora.com.br -->
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:511515,hjsv:5};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
