0206c840849b9f335536b2e8b92d01d49564c29a
Igor Prado <contato@igorprado.com.br>
2017-10-04 10:06:06 -0300

    Remoção de tag de conversão

--- footer.php
+++ footer.php
@@ -82,19 +82,3 @@ Política de Privacidade
 	</div>
 
 </div>
-
-<script type="text/javascript">
-/* <![CDATA[ */
-var google_conversion_id = 882192095;
-var google_conversion_label = "t6y9COXCp3AQ393UpAM";
-var google_custom_params = window.google_tag_params;
-var google_remarketing_only = true;
-/* ]]> */
-</script>
-<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
-</script>
-<noscript>
-<div style="display:inline;">
-<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/882192095/?value=1.00&amp;currency_code=BRL&amp;label=t6y9COXCp3AQ393UpAM&amp;guid=ON&amp;script=0"/>
-</div>
-</noscript>
