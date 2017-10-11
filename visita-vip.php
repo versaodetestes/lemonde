9ad18cb14a8041205b590c47d5439d56c94a30f4
Igor Prado <contato@igorprado.com.br>
2017-10-11 10:09:09 -0300

    mudança de icone - visita vip

--- visita-vip.php
+++ visita-vip.php
@@ -2,11 +2,13 @@
 <html lang="pt_BR">
 <head>
 	<meta charset="UTF-8">
-	<title>Visita VIP - Le Monde Campolim</title>
+	<title>Visita ao Decorado - Le Monde Campolim</title>
+
 	<link rel="stylesheet" type="text/css" href="assets/css/style-contato.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/datedropper.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<!-- HEAD -->
-	<?php include ("head.php"); ?>
+	<?php include ("head.php"); ?>	
 </head>
 <body>
 	<!-- HEADER -->
@@ -14,20 +16,58 @@
 	<!-- FIM HEADER -->
 	<div class="container">
 		<div class="row justify-content-center">
+			<div class="col-md-7">
 				<div class="title-group text-center">
-					<h1>Agendar visita VIP</h1>
-					<p class="lead">Uma experiência única</p>
-					<span class="line line-blue"></span><br>
-					<a href="tel:1533187999"><span class="icon-tel-contato"><img src="assets/img/telefone.svg"></span><span class="telefone">(15) 3318-7999</span></a>
+					<h1>Agendar Visita VIP</h1>
+					<p>Rua Antônio Perez Hernandez, 333<br>
+						Campolim, Sorocaba/SP
+					</p>
 				</div>
-		</div>
-				<div class="row">
+			</div>
 
-			<div class="col-md-6">
-				<img src="assets/img/stand-vendas.jpg" class="img-fluid reveal">
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12  text-center">
+				<img src="assets/img/visite-decorado-1.jpg" class="img-fluid rounded" style="margin-bottom: 50px;">
+			</div>
+			
+		</div>
+	</div>
+
+	<div class="container">
+
+	<div class="row justify-content-center">
+		<div class="col-md-6">
+			<h2>Vantagens da visita Vip</h2>
+			<p>
+			<span class="vip-vantagens"><strong>Maquete:</strong> Conhecer a maquete física é essencial pra compreender em três dimensões a totalidade do empreendimento. Tenha uma visão geral da implantação, das áreas de lazer, garagens e fluxo de carros.</span>	
+			</p>
+			<p>
+			<span class="vip-vantagens"><strong>Decorado:</strong> O imóvel decorado é uma grande fonte de ideias para você aplicar no seu novo apartamento. Aproveitamento de espaço, truques de decoração e outros insights podem vir da sua visita ao stand.</span>
+			</p>
+			<p>
+			<span class="vip-vantagens"><strong>Apresentação Multimídia:</strong> Uma apresentação detalhista criada para que você entenda nos mínimos detalhes todos os aspectos do projeto.</span>
+			</p>
+
+			<p>
+			<span class="vip-vantagens"><strong>Dúvidas:</strong> É importante que, durante sua visita, você tire todas as dúvidas com o corretor. Medidas, memorial descritivo, possibilidades de alteração de estrutura, vista do apartamento, etc. </span>
+			</p>
+		</div>
+	</div>
+		<div class="row justify-content-center">
+			<div class="col-md-12 text-center">
+				<img src="assets/img/visita-vip-title.png" class="img-fluid" style="margin-top: 50px;">
+
 			</div>
 			<div class="col-md-6">
-				<form class="form-contact" role="form" id="form-email" action="send-email-vip.php" method="POST">
+				<div class="info">
+					<p>
+						Você receberá uma confirmação por telefone ou por e-mail para a data e horário escolhido.
+					</p>
+				</div>
+				<form class="form-contact" role="form" id="form-email" action="send-email-decorado.php" method="POST">
 				  <div class="form-group">
 				    <label for="nome">Nome</label>
 				    <input type="text" class="form-control" id="nome" name="nome"  placeholder="Digite seu nome">
@@ -37,51 +77,34 @@
 				    <input type="email" class="form-control" id="email" name="email"  placeholder="Digite seu principal email">
 				  </div>
 				  <div class="form-group">
-				    <label for="telefone">Telefone</label>
-				    <input type="tel" class="form-control" id="telefone" name="telefone"  placeholder="Digite seu principal telefone">
-				  </div>
-				  <div class="form-group">
 				    <label for="celular">WhatsApp / Celular</label>
 				    <input type="tel" class="form-control" id="celular" name="celular"  placeholder="Digite seu principal celular">
 				  </div>
 				  <div class="form-group">
-				    <label for="estado">Estado</label>
-				    <select class="form-control" name="estado" id="estado">
-				    	<option>Selecione um Estado</option>
-				    </select>
+				    <label for="data">Data</label>
+				    <input type="date" class="form-control" id="data" name="data" data-lang="pt" data-format="d M, Y" data-large-mode="true" data-large-default="true" data-min-year="2017" >
 				  </div>
 				  <div class="form-group">
-				    <label for="cidade">Cidade</label>
-				    <select class="form-control" name="cidade" id="cidade">
-				    	<option>Selecione uma Cidade</option>
-				    </select>
-				  </div>
-				  <div class="form-group">
-				    <label for="planta">Selecione seu apartamento ideal</label>
-				    <select class="form-control" name="planta" id="planta">
-				    	<option>Selecione uma planta</option>
-				    	<option value="Apto 48m²">Apto 48m²</option>
-				    	<option value="Apto 62m²">Apto 62m²</option>
-				    	<option value="Duplex 101m²">Duplex 101m²</option>
-				    	<option value="Duplex 129m²">Duplex 129m²</option>
-				    </select>
+				  	<label for="horario">Horário</label>
+				  	<select class="form-control" id="horario" name="horario">
+				  		<option value="09:00">09:00</option>
+				  		<option value="10:00">10:00</option>
+				  		<option value="11:00">11:00</option>
+				  		<option value="12:00">12:00</option>
+				  		<option value="13:00">13:00</option>
+				  		<option value="14:00">14:00</option>
+				  		<option value="15:00">15:00</option>
+				  		<option value="16:00">16:00</option>
+				  		<option value="17:00">17:00</option>
+				  		<option value="18:00">18:00</option>
+				  	</select>
 				  </div>
 
  					<div class="form-group">
 				    <label for="mensagem">Dúvidas e informações adicionais</label>
 				    <textarea rows="2" class="form-control" id="mensagem" name="mensagem"></textarea>
 					</div>
-				  <label style="margin-right: 10px;">Deseja receber contato via:</label><br class="visible-xs">
-				  <label style="margin-right: 20px;" for="emailcheck" style="margin-top: 5px;"><input type="checkbox" name="emailcheck" value="Email" id="emailcheck" checked="checked">
-						Email
-					</label>
-				  <label style="margin-right: 20px;" for="whatsappcheck" style="margin-top: 5px;"><input type="checkbox" value="WhatsApp" name="whatsappcheck" id="whatsappcheck">
-						WhatsApp
-					</label>
-				  <label style="margin-right: 20px;" for="telefonecheck" style="margin-top: 5px;"><input type="checkbox" value="Telefone" name="telefonecheck" id="telefonecheck">
-						Telefone
-					</label><br>
-					<button type="submit" class="btn btn-cta">Enviar mensagem</button>
+					<button type="submit" class="btn btn-cta">Agendar visita</button>
 					<label for="autorizo" style="margin-top: 5px;"><input type="checkbox" name="autorizacao" id="autorizo" checked="checked">
 						<small>Autorizo receber informações de CRB Construtora. Nunca enviamos spam.</small>
 					</label>
@@ -89,6 +112,7 @@
 				</form>
 			</div>
 		</div>
+
 
 	</div>
 
@@ -98,28 +122,14 @@
 		<!-- FIM FOOTER -->
 
 <script type="text/javascript" src="assets/js/jquery-1.9.1.min.js"></script>
-<script src="assets/js/cidades-estados.js" charset='iso-8859-1'></script>
-<script type="text/javascript">
-	
-// CIDADE E ESTADO
-    $(document).ready(function(){
-        new dgCidadesEstados( 
-            document.getElementById('estado'), 
-            document.getElementById('cidade'), 
-            true
-        );
-        new dgCidadesEstados( 
-            document.getElementById('estado-2'), 
-            document.getElementById('cidade-2'), 
-            true
-        );
-    });
-</script>
 <script type="text/javascript" src="assets/js/main-script.js"></script>
-	<script type="text/javascript" src="https://d335luupugsy2.cloudfront.net/js/integration/stable/rd-js-integration.min.js"></script> 
-  <script type="text/javascript">
-  RdIntegration.integrate('4928a2b2f6bbc166823f44e7f99295c2', 'LP - Le Monde - Visita VIP'); 
-  </script>
-
+<script type="text/javascript" src="assets/js/datedropper.min.js"></script>
+<script type="text/javascript" src="https://d335luupugsy2.cloudfront.net/js/integration/stable/rd-js-integration.min.js"></script> 
+<script type="text/javascript">
+RdIntegration.integrate('4928a2b2f6bbc166823f44e7f99295c2', 'LP - Le Monde - Visita Vip'); 
+</script>
+<script>
+$('#data').dateDropper();
+</script>
 </body>
 </html>	
